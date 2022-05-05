c66a4ad147197dd4fd2a477a722e7221c4eb1186
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-05-05 11:37:32 -0700

    setup index.js, app.js, and initial views folders.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import { Route, Switch } from 'react-router-dom';
+import Auth from './views/Auth/Auth';
+import Home from './views/Home/Home';
+import Dashboard from './views/Users/Dashboard';
+
+
 export default function App() {
   return <h1>Hello World</h1>;
 }
